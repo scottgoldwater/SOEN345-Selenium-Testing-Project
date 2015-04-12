3ac1de587e55cf9b70265040e7ce7aaa7f3539fb
Mark Karanfil <m.karanf@gmail.com>
2015-04-12 16:19:49 -0400

    UC-3 Successful Case Unit Test Added
    The system test for the successful flow of UC-3, "Successfully Cancel a
    room booking", was added. It adds a booking to delete before the test is
    run, and checks that the slot where the booking was added is empty after
    the test by asserting that its href attribute is what is expected if the
    slot is empty.

---
+++ seleniumTest/src/test/java/TestMeetingRoomBookingSystem/TestCancellingARoomBooking.java
@@ -0,0 +1,51 @@
+package TestMeetingRoomBookingSystem;
+
+import TestMeetingRoomBookingSystem.Rules.LogInRule;
+import static org.junit.Assert.*;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by User on 4/12/2015.
+ */
+public class TestCancellingARoomBooking {
+    private final String baseURL = "http://donatepls.com/mrbs-1.4.11/web/";
+    private final String dayURL = baseURL + "day.php?year=2015&month=4&day=10&area=1&room=4";
+    private String date = "04/10/2010";
+    private String xPath = "/html/body/div[2]/table[1]/tbody/tr[1]/td[4]/div/a";
+    private String name = "toDelete";
+
+    @Rule
+    public LogInRule session = new LogInRule();
+
+    @Before
+    public void createRoom(){
+        session.driver.navigate().to(dayURL);
+        session.driver.findElement(By.xpath(xPath)).click();
+        session.driver.findElement(By.id("name")).sendKeys(name);
+        session.driver.findElement(By.id("description")).sendKeys(name);
+
+        session.driver.findElement(By.name("save_button")).click();
+
+    }
+
+    @Test
+    public void standardFlowTest(){
+        // Find booking with the title given when creating the room
+        List<WebElement> rooms = session.driver.findElements(By.xpath("//a[@title='" + name + "']"));
+        for (WebElement e : rooms){
+            if (e.isDisplayed())
+                e.click();
+        }
+        session.driver.findElement(By.id("view_entry_nav")).findElements(By.tagName("div")).get(1).findElement(By.tagName("a")).click();
+        session.driver.switchTo().alert().accept();
+        WebElement bookingSlot = session.driver.findElement(By.xpath("/html/body/div[2]/table[1]/tbody/tr[1]/td[4]/div/a"));
+        // Assert that the slot the booking was added to now links to the expected page when empty.
+        assertEquals(baseURL + "edit_entry.php?area=1&room=3&hour=7&minute=0&year=2015&month=4&day=10", bookingSlot.getAttribute("href"));
+    }
+}
