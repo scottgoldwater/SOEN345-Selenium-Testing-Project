0ff3647b0d32aabb9398b23e8a8b5f45b01d98b3
Mark Karanfil <m.karanf@gmail.com>
2015-04-12 20:00:23 -0400

    UC-5 Success Flow Test Added
    Added the successful flow system test of UC-5, Modify Room Properties.
    Assertions are made to ensure that the values of the room we
    successfully changed. The test assumes that the room created of
    modification for the purposes of the test is added to the second row of
    the rooms table

---
+++ seleniumTest/src/test/java/TestMeetingRoomBookingSystem/TestModifyARoom.java
@@ -0,0 +1,91 @@
+package TestMeetingRoomBookingSystem;
+
+import org.junit.*;
+import java.util.List;
+import static org.junit.Assert.*;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by User on 4/12/2015.
+ */
+public class TestModifyARoom {
+
+    private WebDriver driver;
+    private final String URL = "http://donatepls.com/mrbs-1.4.11/web/admin.php";
+    private final String USERNAME = "admin";
+    private final String PASSWORD = "admin";
+    private final String ROOMNAME = "A Room To Modify";
+    private final String DESCRIPTION = "This room will be modified";
+    private final String CAPACITY = "5";
+    private final String MOD_ROOMNAME = "A Modified Room";
+    private final String MOD_DESCRIPTION = "The room was modified";
+    private final String MOD_CAPACITY = "6";
+    private final String MOD_EMAIL = "mod@hotmail.com";
+
+    @Before
+    public void setUp() throws Exception {
+        // Log in as an Admin
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.navigate().to(URL);
+        driver.findElement(By.id("NewUserName")).sendKeys(USERNAME);
+        driver.findElement(By.id("NewUserPassword")).sendKeys(PASSWORD);
+        driver.findElement(By.id("NewUserPassword")).sendKeys(Keys.ENTER);
+
+        // Add the room to modify
+        driver.findElement(By.linkText("Rooms")).click();
+        driver.findElement(By.id("room_name")).clear();
+        driver.findElement(By.id("room_name")).sendKeys(ROOMNAME);
+        driver.findElement(By.id("room_description")).clear();
+        driver.findElement(By.id("room_description")).sendKeys(DESCRIPTION);
+        driver.findElement(By.id("room_capacity")).clear();
+        driver.findElement(By.id("room_capacity")).sendKeys(CAPACITY);
+        driver.findElement(By.xpath("//input[@value='Add Room']")).click();
+    }
+
+    @Test
+    public void standardFlowTest(){
+        // Keep track of the number of enabled rooms before the modification
+        int numOfCheckmarksBefore = driver.findElements(By.xpath("//img[@alt='check mark']")).size();
+
+        driver.findElement(By.xpath("//a[@title='" + ROOMNAME + "']")).click();
+
+        // Modify The room
+        driver.findElement(By.id("room_name")).clear();
+        driver.findElement(By.id("room_name")).sendKeys(MOD_ROOMNAME);
+        driver.findElements(By.name("room_disabled")).get(1).click(); // Click second radio button option
+        driver.findElement(By.id("description")).clear();
+        driver.findElement(By.id("description")).sendKeys(MOD_DESCRIPTION);
+        driver.findElement(By.id("capacity")).clear();
+        driver.findElement(By.id("capacity")).sendKeys(MOD_CAPACITY);
+        driver.findElement(By.id("room_admin_email")).clear();
+        driver.findElement(By.id("room_admin_email")).sendKeys(MOD_EMAIL);
+        driver.findElement(By.xpath("//input[@name='change_room']")).click();
+
+        String act_mod_name = driver.findElement(By.xpath("//a[@title='" + MOD_ROOMNAME + "']")).getText();
+        int numOfCheckmarksAfter = driver.findElements(By.xpath("//img[@alt='check mark']")).size();
+        String act_mod_description = driver.findElement(By.xpath("//table[@id='rooms_table']/tbody/tr[2]/td[2]/div")).getText();
+        String act_mod_capacity = driver.findElement(By.xpath("//table[@id='rooms_table']/tbody/tr[2]/td[3]/div")).getText();
+        String act_mod_email = driver.findElement(By.xpath("//table[@id='rooms_table']/tbody/tr[2]/td[4]/div")).getText();
+
+        assertEquals(MOD_ROOMNAME, act_mod_name);
+        assertEquals(1, numOfCheckmarksBefore - numOfCheckmarksAfter); //should be one less enabled checkmark
+        assertEquals(MOD_DESCRIPTION, act_mod_description);
+        assertEquals(MOD_CAPACITY, act_mod_capacity);
+        assertEquals(MOD_EMAIL, act_mod_email);
+    }
+
+    @After
+    public void tearDown(){
+        // Delete second row where the added room is placed
+        driver.findElements(By.xpath("//img[@alt='Delete']")).get(1).click();
+        driver.findElement(By.id("del_yes")).click();
+    }
+
+}
