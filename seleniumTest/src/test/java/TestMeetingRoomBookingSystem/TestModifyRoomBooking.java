e438c498533f7319ac60d55ad2710a17a3bc1140
Scott Goldwater <scottgoldwater@gmail.com>
2015-04-12 23:00:10 -0400

    Finished UC-2 altFlowTestUserDidNotCreate 1a

--- seleniumTest/src/test/java/TestMeetingRoomBookingSystem/TestModifyRoomBooking.java
+++ seleniumTest/src/test/java/TestMeetingRoomBookingSystem/TestModifyRoomBooking.java
@@ -13,46 +13,34 @@ public class TestModifyRoomBooking {
     @Rule public LogInRule session = new LogInRule("http://donatepls.com/mrbs-1.4.11/web/admin.php","rob","123");
 
     private String date = "04/12/2010";
-    private String xPath = "/html/body/div[2]/table[1]/tbody/tr[1]/td[2]/div/a";
     private String name;
     private String description;
-
 
     @Before
     public void createRoom(){
         name = "Homer";
         description = "Simpson";
-        session.driver.findElement(By.className("dateselector")).findElement(By.id("datepicker")).sendKeys(Keys.chord(Keys.CONTROL, "A"), date, Keys.ENTER);
-        session.driver.findElement(By.xpath(xPath)).click();
+        gotoDate();
+        session.driver.findElement(By.xpath("/html/body/div[2]/table[1]/tbody/tr[1]/td[2]/div/a")).click();
         session.driver.findElement(By.id("name")).sendKeys(name);
         session.driver.findElement(By.id("description")).sendKeys(description);
 
         session.driver.findElement(By.name("save_button")).click();
-
     }
 
     @After
     public void deleteRoom(){
 
-        session.driver.findElement(By.className("dateselector")).findElement(By.id("datepicker")).sendKeys(Keys.chord(Keys.CONTROL, "A"), date, Keys.ENTER);
-        List<WebElement> rooms = session.driver.findElements(By.xpath("//a[@title='"+description+"']"));
-        for (WebElement e : rooms){
-            if (e.isDisplayed())
-                e.click();
-        }
+        gotoDate();
+        selectBooking();
+
         session.driver.findElement(By.id("view_entry_nav")).findElements(By.tagName("div")).get(1).findElement(By.tagName("a")).click();
         session.driver.switchTo().alert().accept();
-
-
     }
 
     @Test
     public void standardFlowTest(){
-        List<WebElement> rooms = session.driver.findElements(By.xpath("//a[@title='"+description+"']"));
-        for (WebElement e : rooms){
-            if (e.isDisplayed())
-                e.click();
-        }
+        selectBooking();
 
         session.driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/a")).click();
         name = "Gandalf";
@@ -62,14 +50,37 @@ public class TestModifyRoomBooking {
         session.driver.findElement(By.id("description")).sendKeys(Keys.chord(Keys.CONTROL, "A"),Keys.chord(Keys.CONTROL, "A") , description);
         session.driver.findElement(By.xpath("/html/body/div[2]/form/fieldset/fieldset/div[2]/input")).click();
 
-        List<WebElement> changedRooms = session.driver.findElements(By.xpath("//a[@title='"+description+"']"));
-        for (WebElement e : changedRooms){
+        selectBooking();
+
+        Assert.assertEquals(session.driver.findElement(By.xpath("/html/body/div[2]/h3")).getText(), name);
+        Assert.assertEquals(session.driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr[1]/td[2]")).getText(), description);
+    }
+
+    @Test
+    public void altFlowOtherUserTest(){
+        session.setCredentials("test", "test123");
+        session.logout();
+        session.login();
+
+        gotoDate();
+        selectBooking();
+        session.driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/a")).click();
+        Assert.assertEquals(session.driver.findElement(By.xpath("/html/body/div[2]/h1")).getText(),"Access Denied");
+        session.logout();
+        session.setCredentials("rob", "123");
+        session.login();
+    }
+
+    public void gotoDate(){
+        session.driver.findElement(By.className("dateselector")).findElement(By.id("datepicker")).sendKeys(Keys.chord(Keys.CONTROL, "A"), date, Keys.ENTER);
+    }
+
+    public void selectBooking(){
+        List<WebElement> rooms = session.driver.findElements(By.xpath("//a[@title='"+description+"']"));
+        for (WebElement e : rooms){
             if (e.isDisplayed())
                 e.click();
         }
-
-        Assert.assertEquals(session.driver.findElement(By.xpath("/html/body/div[2]/h3")).getText(),name);
-        Assert.assertEquals(session.driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr[1]/td[2]")).getText(),description);
     }
 
 }
